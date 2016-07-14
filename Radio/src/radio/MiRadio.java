08423d7969d597d849ea9a9b41b9a380473fce51
tez15417 <tez15417@uvg.edu.gt>
2016-07-14 14:17:41 -0600

    Update MiRadio.java

--- Radio/src/radio/MiRadio.java
+++ Radio/src/radio/MiRadio.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package radio;
 
 /**
- *
+ * Jorge Mario Tezen Cristales 15417
  * Diego Fernando Rivera Escobar 15085
  * Programación orientada a objetos
  */
